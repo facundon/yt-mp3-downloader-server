f0325d30e01a721ebc1baf7a8d7381ae1358b7bc
Facundo Rodriguez <facundonrodriguez@gmail.com>
2021-06-04 16:03:30 -0300

    Rework some routes to use params

--- src/controller/addFavorite.ts
+++ src/controller/addFavorite.ts
@@ -5,13 +5,14 @@ import { Video } from "../models/Video"
 import { checkVideoDuplicates } from "../utils"
 
 export async function addFavorite(req: Request, res: Response) {
+   if (!req.params.id || !req.body.title) return
    const userId = req.user.id
    const user = await getRepository(User).findOne({ id: userId })
    if (!userId || !user) {
       res.status(500).json({ message: "Couldn't find user" })
       return
    }
-   const isDuplicated = await checkVideoDuplicates(req.body.id, user)
+   const isDuplicated = await checkVideoDuplicates(req.params.id, user)
    if (isDuplicated) {
       res.status(400).json({ message: "User already has that video" })
       return
@@ -20,7 +21,7 @@ export async function addFavorite(req: Request, res: Response) {
    const videoRepo = getRepository(Video)
    try {
       const newFav = videoRepo.create({
-         videoId: req.body.id,
+         videoId: req.params.id,
          title: req.body.title,
          user: userId,
       })
